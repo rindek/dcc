94d4046f8d98d302c07ac81c62bb948f085662e9
Jacek Jakubik <jakubik.jacek@gmail.com>
2017-06-27 23:53:43 +0200

    Add couple more tests

--- v1tov32_test.go
+++ v1tov32_test.go
@@ -34,4 +34,53 @@ var _ = Describe("v1tov32", func() {
 			}
 		})
 	})
+
+	Describe("PortRange validate", func() {
+		Context("invalid port range", func() {
+			pr := PortRange{
+				Start: 100,
+				End:   50,
+			}
+
+			f := func() {
+				pr.validate("test")
+			}
+
+			It("panics", func() {
+				Expect(f).To(Panic())
+			})
+		})
+
+		Context("valid port range", func() {
+			pr := PortRange{
+				Start: 100,
+				End:   150,
+			}
+
+			f := func() { pr.validate("test") }
+
+			It("does not panic", func() {
+				Expect(f).NotTo(Panic())
+			})
+		})
+	})
+
+	Describe("extractRegexpVars", func() {
+		re := createRegexp(`^(?P<volume_name>[a-z0-9-_]+):(?P<container_path>.+)$`)
+
+		str := "test:/var/www"
+
+		It("expects to match the test string", func() {
+			Expect(re.MatchString(str)).To(BeTrue())
+		})
+
+		exp := map[string]string{
+			"volume_name":    "test",
+			"container_path": "/var/www",
+		}
+
+		It("expects to properly extract the vars", func() {
+			Expect(extractRegexpVars(re, &str)).To(Equal(exp))
+		})
+	})
 })
